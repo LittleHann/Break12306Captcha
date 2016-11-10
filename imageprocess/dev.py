980a1e152655f39d6e8096fbc7e03097eafb44d9
haonans <shn2013@outlook.com>
2016-11-10 16:03:43 -0500

    change to packages for import compatability and added a script to generate triple training sets

--- imageprocess/dev.py
+++ imageprocess/dev.py
@@ -1,23 +1,21 @@
 from PIL import Image
 from PIL.ImageOps import grayscale
-from utils import cropLabel, resize
 import numpy as np
 import matplotlib.pyplot as plt
 
+from imageprocess.utils import trimLabel, cropLabel
 
 np.set_printoptions(threshold=np.nan)
 
-
 print "Demo for cropping generated label"
-im = Image.open("data/mh.png")
+im = Image.open("../data/mh.png")
 label = trimLabel(im, size=100)
 label = label.resize((60, 60))
 label.show()
 raw_input("Press Enter to Continue")
 
-
 print "Demo for cropping real label"
-im = Image.open("data/captcha_1.jpg")
+im = Image.open("../data/captcha_0.jpg")
 label = cropLabel(im)
 label = trimLabel(label, size=100)
 label = label.resize((60, 60))
