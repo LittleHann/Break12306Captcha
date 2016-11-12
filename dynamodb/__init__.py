47897e4224081d660ce8fa2f44423d1b4d3f3d4e
haonans <shn2013@outlook.com>
2016-11-12 17:04:46 -0500

    Connect to DynamoDB using boto3 (please change policies first)

---
+++ dynamodb/__init__.py
@@ -0,0 +1,42 @@
+"""
+http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.JsShell.html#GettingStarted.JsShell.Prereqs.Download
+http://docs.aws.amazon.com/amazondynamodb/latest/gettingstartedguide/GettingStarted.Python.html
+
+"""
+import boto3
+import json
+
+# - http://boto3.readthedocs.io/en/latest/guide/configuration.html
+credentials = json.load(open('../aws/credentials.json'))
+dynamodb = boto3.client('dynamodb', aws_access_key_id=credentials['AWS_ACCESS_KEY_ID'],
+                        aws_secret_access_key=credentials['AWS_SECRET_ACCESS_KEY'])
+
+table = dynamodb.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  # Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  # Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+
