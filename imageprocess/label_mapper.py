098a2e4b7a98b9bb838b97b615e33608e35c8285
Heqing Ya <normanyahq@gmail.com>
2016-11-28 19:44:27 -0500

    label mapper

--- imageprocess/label_mapper.py
+++ imageprocess/label_mapper.py
@@ -7,7 +7,8 @@ from itertools import izip
 import utils
 from multiprocessing import Process
 import tensorflow as tf
-
+from collections import deque
+import traceback
 
 np.set_printoptions(threshold=np.nan)
 PIXEL_DEPTH = 255
@@ -41,7 +42,7 @@ def label_converter(file_path, separator='\t'):
     '''
     captcha = Image.open(file_path)
     label = utils.crop_label(captcha)
-    label = np.array(utils.trim_label(label).resize((60, 60))).reshape(60**2)
+    label = np.array(utils.trim_label(label).resize((60, 60)))
     label = (label - PIXEL_DEPTH / 2.0) / PIXEL_DEPTH
     return label
 
@@ -59,180 +60,205 @@ def label_mapper(file_path, separator='\t'):
     result_list.append(np.array_str(label, max_line_width=100000))
     return separator.join(result_list)
 
-def worker(file_list, output_dir, total_workers, worker_id):
-    f = open(os.path.join(output_dir, "output_%d.txt" % worker_id), "w")
-    for path in file_list:
-        if hash(path) % total_workers == worker_id:
-            f.write(captcha_mapper(path) + '\n')
-    f.close()
+def worker(model_path, file_list, output_dir, total_workers, worker_id, debug=False):
+    train_data_node = tf.placeholder(
+          data_type(),
+          shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+    train_labels_node = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
+    eval_data = tf.placeholder(
+          data_type(),
+          shape=(EVAL_BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+
+    # The variables below hold all the trainable weights. They are passed an
+    # initial value which will be assigned when we call:
+    # {tf.initialize_all_variables().run()}
+    conv1_weights = tf.Variable(
+          tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
+                              stddev=0.1,
+                              seed=SEED, dtype=data_type()))
+    conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
+    conv2_weights = tf.Variable(tf.truncated_normal(
+          [5, 5, 32, 64], stddev=0.1,
+          seed=SEED, dtype=data_type()))
+    conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
+    fc1_weights = tf.Variable(  # fully connected, depth 512.
+          tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
+                              stddev=0.1,
+                              seed=SEED,
+                              dtype=data_type()))
+    fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
+    fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
+                                                    stddev=0.1,
+                                                    seed=SEED,
+                                                    dtype=data_type()))
+    fc2_biases = tf.Variable(tf.constant(
+          0.1, shape=[NUM_LABELS], dtype=data_type()))
+
+    # We will replicate the model structure for the training subgraph, as well
+    # as the evaluation subgraphs, while sharing the trainable parameters.
+    def model(data, train=False):
+        """The Model definition."""
+        # 2D convolution, with 'SAME' padding (i.e. the output feature map has
+        # the same size as the input). Note that {strides} is a 4D array whose
+        # shape matches the data layout: [image index, y, x, depth].
+        conv = tf.nn.conv2d(data,
+                            conv1_weights,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME')
+        # Bias and rectified linear non-linearity.
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+        # Max pooling. The kernel size spec {ksize} also follows the layout of
+        # the data. Here we have a pooling window of 2, and a stride of 2.
+        pool = tf.nn.max_pool(relu,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
+        conv = tf.nn.conv2d(pool,
+                            conv2_weights,
+                            strides=[1, 1, 1, 1],
+                            padding='SAME')
+        relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
+        pool = tf.nn.max_pool(relu,
+                              ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1],
+                              padding='SAME')
+        # Reshape the feature map cuboid into a 2D matrix to feed it to the
+        # fully connected layers.
+        pool_shape = pool.get_shape().as_list()
+        reshape = tf.reshape(
+            pool,
+            [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+        # Fully connected layer. Note that the '+' operation automatically
+        # broadcasts the biases.
+        hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
+        # Add a 50% dropout during training only. Dropout also scales
+        # activations such that no rescaling is needed at evaluation time.
+        if train:
+          hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
+        return tf.matmul(hidden, fc2_weights) + fc2_biases
+
+    # Training computation: logits + cross-entropy loss.
+    logits = model(train_data_node, True)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+          logits, train_labels_node))
+
+    # L2 regularization for the fully connected parameters.
+    regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases) +
+                      tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
+    # Add the regularization term to the loss.
+    loss += 5e-4 * regularizers
+
+    # Optimizer: set up a variable that's incremented once per batch and
+    # controls the learning rate decay.
+    batch = tf.Variable(0, dtype=data_type())
+    # Decay once per epoch, using an exponential schedule starting at 0.01.
+    learning_rate = tf.train.exponential_decay(
+          0.01,                # Base learning rate.
+          batch * BATCH_SIZE,  # Current index into the dataset.
+          1,          # Decay step.
+          0.95,                # Decay rate.
+          staircase=True)
+    # Use simple momentum for the optimization.
+    # optimizer = tf.train.MomentumOptimizer(learning_rate,
+    #                                        0.9).minimize(loss,
+    #                                                      global_step=batch)
+
+    # Predictions for the current training minibatch.
+    train_prediction = tf.nn.softmax(logits)
+
+    # Predictions for the test and validation, which we'll compute less often.
+    eval_prediction = tf.nn.softmax(model(eval_data))
+
+    # Small utility function to evaluate a dataset by feeding batches of data to
+    # {eval_data} and pulling the results from {eval_predictions}.
+    # Saves memory and enables this to run on smaller GPUs.
+    def eval_in_batches(data, sess):
+        """Get all predictions for a dataset by running it in small batches."""
+        size = data.shape[0]
+        if size < EVAL_BATCH_SIZE:
+          raise ValueError("batch size for evals larger than dataset: %d" % size)
+        predictions = np.ndarray(shape=(size, NUM_LABELS), dtype=np.float32)
+        for begin in xrange(0, size, EVAL_BATCH_SIZE):
+          end = begin + EVAL_BATCH_SIZE
+          if end <= size:
+            predictions[begin:end, :] = sess.run(
+                eval_prediction,
+                feed_dict={eval_data: data[begin:end, ...]})
+          else:
+            batch_predictions = sess.run(
+                eval_prediction,
+                feed_dict={eval_data: data[-EVAL_BATCH_SIZE:, ...]})
+            predictions[begin:, :] = batch_predictions[begin - size:, :]
+        return predictions
 
 
-train_data_node = tf.placeholder(
-      data_type(),
-      shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
-train_labels_node = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
-eval_data = tf.placeholder(
-      data_type(),
-      shape=(EVAL_BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+        saver.restore(sess, model_path)
+        if debug:
+            print('Initialized!')
+        f = open(os.path.join(output_dir, "output_%d.txt" % worker_id), "w")
+        q = deque()
+        predictions = np.ndarray(shape=(BATCH_SIZE, NUM_LABELS), dtype=np.float32)
+        data = np.ndarray(shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS), dtype=np.float32)
+        for idx, path in enumerate(file_list):
+            if hash(path) % total_workers == worker_id:
+                try:
+                    label = label_converter(path)
+                except:
+                    traceback.print_exc()
+                    continue
+                file_name = os.path.basename(path)
+                data[len(q), :, :, 0] = label
+                q.append(file_name)
+                if len(q) == BATCH_SIZE:
+                    predictions = sess.run(
+                            eval_prediction,
+                            feed_dict={eval_data: data})
 
-# The variables below hold all the trainable weights. They are passed an
-# initial value which will be assigned when we call:
-# {tf.initialize_all_variables().run()}
-conv1_weights = tf.Variable(
-      tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
-                          stddev=0.1,
-                          seed=SEED, dtype=data_type()))
-conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
-conv2_weights = tf.Variable(tf.truncated_normal(
-      [5, 5, 32, 64], stddev=0.1,
-      seed=SEED, dtype=data_type()))
-conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
-fc1_weights = tf.Variable(  # fully connected, depth 512.
-      tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
-                          stddev=0.1,
-                          seed=SEED,
-                          dtype=data_type()))
-fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
-fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
-                                                stddev=0.1,
-                                                seed=SEED,
-                                                dtype=data_type()))
-fc2_biases = tf.Variable(tf.constant(
-      0.1, shape=[NUM_LABELS], dtype=data_type()))
+                    for i, fname in enumerate(q):
+                        f.write("%d\t%s\n" % (i, np.array_str(predictions[i, :], max_line_width=100000)))
+                        if debug:
+                            print ("%s\t%s\t%.2f" % (fname, np.array_str(np.argmax(predictions[i, :])), np.max(predictions[i, :])))
+                    q = deque()
+        if q:
+            predictions = sess.run(
+                              eval_prediction,
+                              feed_dict={eval_data: data})
+        for i, fname in enumerate(q):
+            f.write("%d\t%s\n" % (i, np.array_str(predictions[i, :], max_line_width=100000)))
+            if debug:
+                print ("%s\t%s\t%.2f" % (fname, np.array_str(np.argmax(predictions[i, :])), np.max(predictions[i, :])))
 
-# We will replicate the model structure for the training subgraph, as well
-# as the evaluation subgraphs, while sharing the trainable parameters.
-def model(data, train=False):
-    """The Model definition."""
-    # 2D convolution, with 'SAME' padding (i.e. the output feature map has
-    # the same size as the input). Note that {strides} is a 4D array whose
-    # shape matches the data layout: [image index, y, x, depth].
-    conv = tf.nn.conv2d(data,
-                        conv1_weights,
-                        strides=[1, 1, 1, 1],
-                        padding='SAME')
-    # Bias and rectified linear non-linearity.
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
-    # Max pooling. The kernel size spec {ksize} also follows the layout of
-    # the data. Here we have a pooling window of 2, and a stride of 2.
-    pool = tf.nn.max_pool(relu,
-                          ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
-                          padding='SAME')
-    conv = tf.nn.conv2d(pool,
-                        conv2_weights,
-                        strides=[1, 1, 1, 1],
-                        padding='SAME')
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
-    pool = tf.nn.max_pool(relu,
-                          ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1],
-                          padding='SAME')
-    # Reshape the feature map cuboid into a 2D matrix to feed it to the
-    # fully connected layers.
-    pool_shape = pool.get_shape().as_list()
-    reshape = tf.reshape(
-        pool,
-        [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
-    # Fully connected layer. Note that the '+' operation automatically
-    # broadcasts the biases.
-    hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
-    # Add a 50% dropout during training only. Dropout also scales
-    # activations such that no rescaling is needed at evaluation time.
-    if train:
-      hidden = tf.nn.dropout(hidden, 0.5, seed=SEED)
-    return tf.matmul(hidden, fc2_weights) + fc2_biases
-
-# Training computation: logits + cross-entropy loss.
-logits = model(train_data_node, True)
-loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-      logits, train_labels_node))
-
-# L2 regularization for the fully connected parameters.
-regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases) +
-                  tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
-# Add the regularization term to the loss.
-loss += 5e-4 * regularizers
-
-# Optimizer: set up a variable that's incremented once per batch and
-# controls the learning rate decay.
-batch = tf.Variable(0, dtype=data_type())
-# Decay once per epoch, using an exponential schedule starting at 0.01.
-learning_rate = tf.train.exponential_decay(
-      0.01,                # Base learning rate.
-      batch * BATCH_SIZE,  # Current index into the dataset.
-      1,          # Decay step.
-      0.95,                # Decay rate.
-      staircase=True)
-# Use simple momentum for the optimization.
-# optimizer = tf.train.MomentumOptimizer(learning_rate,
-#                                        0.9).minimize(loss,
-#                                                      global_step=batch)
-
-# Predictions for the current training minibatch.
-train_prediction = tf.nn.softmax(logits)
-
-# Predictions for the test and validation, which we'll compute less often.
-eval_prediction = tf.nn.softmax(model(eval_data))
-
-# Small utility function to evaluate a dataset by feeding batches of data to
-# {eval_data} and pulling the results from {eval_predictions}.
-# Saves memory and enables this to run on smaller GPUs.
-def eval_in_batches(data, sess):
-    """Get all predictions for a dataset by running it in small batches."""
-    size = data.shape[0]
-    if size < EVAL_BATCH_SIZE:
-      raise ValueError("batch size for evals larger than dataset: %d" % size)
-    predictions = numpy.ndarray(shape=(size, NUM_LABELS), dtype=numpy.float32)
-    for begin in xrange(0, size, EVAL_BATCH_SIZE):
-      end = begin + EVAL_BATCH_SIZE
-      if end <= size:
-        predictions[begin:end, :] = sess.run(
-            eval_prediction,
-            feed_dict={eval_data: data[begin:end, ...]})
-      else:
-        batch_predictions = sess.run(
-            eval_prediction,
-            feed_dict={eval_data: data[-EVAL_BATCH_SIZE:, ...]})
-        predictions[begin:, :] = batch_predictions[begin - size:, :]
-    return predictions
+        f.close()
 
 
-saver = tf.train.Saver()
-with tf.Session() as sess:
-    tf.initialize_all_variables().run()
-    saver.restore(sess, '/home/ubuntu/data/original_tuned__model.ckpt')
-    print('Initialized!')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", dest="debug", type=int, help="enable single group debug mode")
+    parser.set_defaults(debug=False)
+    parser.add_argument("model_path", action="store", help="the path to model")
+    parser.add_argument("file_list_path", action="store",
+                        help="the file list of filenames")
+    parser.add_argument("file_dir", action="store",
+                        help="the path to CAPTCHA file directory")
+    parser.add_argument("output_dir", action="store", help="the output directory")
+    parser.add_argument("n", type=int,
+                        help="the number of process to start")
 
-label = label_converter("/home/ubuntu/data/Break12306Captcha/data/captcha_0.jpg")
-data = label.reshape((1, label.size))
-predictions = numpy.ndarray(shape=(1, NUM_LABELS), dtype=numpy.float32)
-for begin in xrange(0, size, EVAL_BATCH_SIZE):
-    end = begin + EVAL_BATCH_SIZE
-    predictions[0, :] = sess.run(
-            eval_prediction,
-            feed_dict={eval_data: data})
-print predictions
-
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser()
-#
-#     parser.add_argument("file_list_path", action="store",
-#                         help="the file list of filenames")
-#     parser.add_argument("file_dir", action="store",
-#                         help="the path to CAPTCHA file directory")
-#     parser.add_argument("output_dir", action="store", help="the output directory")
-#     parser.add_argument("n", type=int,
-#                         help="the number of process to start")
-#     args = parser.parse_args()
-#     with open(args.file_list_path) as f:
-#         filenames = f.read().strip().split('\n')
-#     file_paths = map(lambda filename: os.path.join(args.file_dir, filename), filenames)
-#     worker_list = list()
-#     for i in xrange(args.n):
-#         worker_list.append(Process(target=worker, args=(file_paths, args.output_dir, args.n, i)))
-#     for p in worker_list:
-#         p.start()
-#     for p in worker_list:
-#         p.join()
+    args = parser.parse_args()
+    with open(args.file_list_path) as f:
+        filenames = f.read().strip().split('\n')
+    file_paths = map(lambda filename: os.path.join(args.file_dir, filename), filenames)
+    worker_list = list()
+    if args.debug is not False:
+        print "in debug mode, group:", args.debug
+        worker(args.model_path, file_paths, args.output_dir, args.n, args.debug, debug=True)
+    else:
+        for i in xrange(args.n):
+            worker_list.append(Process(target=worker, args=(args.model_path, file_paths, args.output_dir, args.n, i)))
+        for p in worker_list:
+            p.start()
+        for p in worker_list:
+            p.join()
