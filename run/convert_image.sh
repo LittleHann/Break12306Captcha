78ecca149a513823c58515ff58a2b853f0600945
Heqing Ya <normanyahq@gmail.com>
2016-12-01 16:36:17 -0500

    fix process number

--- run/convert_image.sh
+++ run/convert_image.sh
@@ -5,7 +5,7 @@ python $SRC_PATH/image_hash/captcha_mapper.py \
   $DATA_PATH/filelist.txt \
   $CAPTCHA_IMAGE \
   $DATA_PATH/tmp_captcha2txt \
-  80
+  24
 
 cat $DATA_PATH/tmp_captcha2txt > $CAPTCHA_TEXT
 rm -r $DATA_PATH/tmp_captcha2txt
