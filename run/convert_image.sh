16403f227e4b84f4617aa0fed0bc93510138835a
Heqing Ya <normanyahq@gmail.com>
2016-12-01 18:31:18 -0500

    the worst thing is you cat a dir and remove them

--- run/convert_image.sh
+++ run/convert_image.sh
@@ -7,5 +7,5 @@ python $SRC_PATH/image_hash/captcha_mapper.py \
   $DATA_PATH/tmp_captcha2txt \
   24
 
-cat $DATA_PATH/tmp_captcha2txt > $CAPTCHA_TEXT
+cat $DATA_PATH/tmp_captcha2txt/*.txt > $CAPTCHA_TEXT
 rm -r $DATA_PATH/tmp_captcha2txt
