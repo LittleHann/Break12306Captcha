44668f5aecdfd5e7ebad7097435ae6abacc632f6
Heqing Ya <normanyahq@gmail.com>
2016-12-03 17:05:33 -0500

    complete front end

---
+++ image_searcher/js/action_bind.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  $("#view-gp-1").click(function() { $(".im-gp-1").viewer(); $(".im-gp-1 img:first").click();})
+  $("#view-gp-2").click(function() { $(".im-gp-2").viewer(); $(".im-gp-2 img:first").click();})
+});
