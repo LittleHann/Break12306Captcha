2a17a5f49a23b5c8e229985709243bb0e115dbe4
Heqing Ya <normanyahq@gmail.com>
2016-12-03 18:46:43 -0500

    add file

--- tools/image_label_prob.py
+++ tools/image_label_prob.py
@@ -1,55 +1,58 @@
-from PIL import Image, ImageDraw
+
 import boto3
 import sys
+import json
+import numpy as np
 import argparse
+import cPickle as pickle
+from collections import defaultdict
 
 BUCKET_NAME = '12306bucket'
 
-def show_captcha(filename, row, col):
-    s3 = boto3.client('s3')
-    s3.download_file(BUCKET_NAME, filename, '/tmp/{}'.format(filename))
-    im = Image.open('/tmp/{}'.format(filename))
-    top = 41 + (67 + 5) * row
-    left = 5 + (67 + 5) * col
-    draw = ImageDraw.Draw(im)
-    corner_size = 10
-    draw.rectangle(((left, top), (left+67, top+67)), outline='red')
-    draw.rectangle(((left, top),
-                    (left+corner_size, top+corner_size)), fill='red')
-    draw.rectangle(((left, top+67-corner_size),
-                    (left+corner_size, top+67)), fill='red')
-    draw.rectangle(((left+67-corner_size, top+67-corner_size),
-                    (left+67, top+67)), fill='red')
-    draw.rectangle(((left+67-corner_size, top),
-                    (left+67, top+corner_size)), fill='red')
-
-    return im
-
-
-def search_captcha(path, phash):
-    result = list()
-    with open (path) as f:
+def load_label_prob(label_prob_path):
+    prob_dict = defaultdict(lambda : defaultdict(float))
+    with open (label_prob_path) as f:
         for line in f:
-            if phash in line:
-                content = line.strip().split('\t')
+            line = line.strip()
+            if line:
+                content = line.split('\t')
                 filename = content[0]
-                idx = content.index(phash)
-                row, col = (idx-2) // 8, (idx//2-1) % 4
-                result.append((filename, row, col))
-    return result
+                for t in content[1:]
+                    idx, v = t.split(":")
+                    idx, v = int(idx), float(v)
+                    prob_dict[filename][idx] = v
+    return prob_dict
+
+def calculate_image_prob(text_captcha_path, rgb2final, prob_dict):
+    image_prob = defaultdict(lambda : defaultdict(float))
+    with open (text_captcha_path) as f:
+        for line in f:
+            content = line.strip().split('\t')
+            filename = content[0]
+            for i in xrange(8):
+                final_rgb = rgb2final[content[i*2+2]]
+                for idx, v in image_prob[file_name].items():
+                    image_prob[final_rgb][idx] += v
+    for rgb in image_prob:
+        s = np.sum(image_prob[rgb].values())
+        for k in image_prob[rgb]:
+            image_prob[rgb][k] /= s
+    return image_prob
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("label_path", action="store",
+    parser.add_argument("label_prob_path", action="store",
                         help="specify the file path for label prediction")
-    parser.add_argument("text_captcha", action="store",
+    parser.add_argument("text_captcha_path", action="store",
                         help="specify the file path for text format CAPTCHA")
     parser.add_argument("mapping_file", action="store",
                         help="specify the file path of mapping from rgb_phash to final rgb_phash")
-
+    parser.add_argument("output", action="store",
+                        help="specify the file path of image category probability")
     args = parser.parse_args()
-    for file_name, row, col in search_captcha(args.text_captcha, args.rgb_phash):
-        print "{} {} {}".format(file_name, row, col)
-        if args.show:
-            show_captcha(file_name, row, col).show()
+
+    label_prob_dict = load_label_prob(args.label_prob_path)
+    rgb2final = pickle.load(open(args.mapping_file))['rgb2final']
+    image_prob = calculate_image_prob(args.text_captcha_path, rgb2final, load_label_prob)
+    json.dump(open(args.output, "w"))
