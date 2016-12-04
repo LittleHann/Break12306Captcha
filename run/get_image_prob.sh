5f537e0360e7f4a933bc83b2786e71f7c14e9cec
Heqing Ya <normanyahq@gmail.com>
2016-12-03 19:00:11 -0500

    add script path

--- run/get_image_prob.sh
+++ run/get_image_prob.sh
@@ -1,2 +1,6 @@
 source $HOME/capstone/Break12306Captcha/run/common.sh
-python image_label_prob.py $LABEL_PROB $CAPTCHA_TEXT $MAPPING_DICT $IMAGE_PROB
+python $SRC_PATH/tools/image_label_prob.py \
+  $LABEL_PROB \
+  $CAPTCHA_TEXT \
+  $MAPPING_DICT \
+  $IMAGE_PROB
