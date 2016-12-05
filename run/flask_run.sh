36e55fb9d446b4d033221f686b65364ca100e84a
Heqing Ya <normanyahq@gmail.com>
2016-12-05 16:46:08 -0500

    fix path problem

--- run/flask_run.sh
+++ run/flask_run.sh
@@ -2,6 +2,6 @@
 source ./common.sh
 python ../image_searcher/app.py \
     $DATA_PATH/rgb_key_2_hashes.pickle \
-    $DATA_PATH/home/haonans/share/hash_2_sources.pickle \
+    $DATA_PATH/hash_2_sources.pickle \
     --host 0.0.0.0 \
     --port 8000 &
