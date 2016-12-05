daa7a7ab390c2d77fa10e5a1ed0fcb5b2be31dab
Heqing Ya <normanyahq@gmail.com>
2016-12-05 03:16:11 -0500

    fix script

--- run/flask_run.sh
+++ run/flask_run.sh
@@ -3,4 +3,5 @@ source ./common.sh
 python ../image_searcher/app.py \
     /data2/haonans/rgb_key_2_hashes.pickle \
     /data2/haonans/hash_2_sources.pickle \
+    --host 0.0.0.0 \
     --port 8000 &
