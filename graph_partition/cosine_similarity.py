418d79cb594d6b2f4766f260e8ca68138e8b6107
Heqing Ya <normanyahq@gmail.com>
2016-12-06 17:10:23 -0500

    better version with aggregation: much faster

--- graph_partition/cosine_similarity.py
+++ graph_partition/cosine_similarity.py
@@ -10,10 +10,11 @@ import json
 
 ALPHA = 0.01
 VEC_SIZE = 4096
+PERCENTILE = 90
 
 def get_sparse_index(vec):
     vec = np.array(vec)
-    threshold = np.percentile(vec, 90)
+    threshold = np.percentile(vec, PERCENTILE)
     idx = np.where(vec > threshold)[0]
     return idx, vec[idx]
 
@@ -28,22 +29,33 @@ def parse_fc7(line):
     t = json.loads(line)
     idx, vec = get_sparse_index(t['fc7'])
     #sys.stderr.write("{}, {}\n".format(len(idx), len(vec)))
-    return ((t['rgb_key'],), SparseVector(VEC_SIZE, idx, vec))
+    return (t['rgb_key'], SparseVector(VEC_SIZE, idx, vec))
 
 
-def get_cosine_similarity(values):
-    p_i, p_j, c_ij, fc7_i, fc7_j = values
-    fc7_i, fc7_j = fc7_i.toArray(), fc7_j.toArray()
-    
+def get_cosine_similarity(line):
+    # line : (p_j, fc7_j, tuples)), tuples: [(p_i, c_ij, fc7_i), ...]
+    p_j, fc7_j, tuple_list = line
+    fc7_j = fc7_j.toArray()
+    result = list()
+    for p_i, c_ij, fc7_i in tuple_list:
+        fc7_i = fc7_i.toArray()
+        weight = (1 - cosine(fc7_i, fc7_j))
+        yield (p_i, (p_j, weight, c_ij))
+        yield (p_j, (p_i, weight, c_ij))
+
     # This cosine is cosine distance,
     # Check https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.distance.cosine.html
-    weight = (1 - cosine(fc7_i, fc7_j))
+
 
     # weight = weight * np.log(c_ij + ALPHA)
     # since the adjustment for alpha is not related to join,
     # we do not calculate it here
     # this is for the efficiency and result reuse
-    return (p_i, p_j, weight)
+
+def expand_result(x):
+    phash, node_list = x
+    result = [t for t in node_list]
+    return phash, result
 
 
 def main(argv):
@@ -61,15 +73,19 @@ def main(argv):
 
     queries = sc.textFile(f_query).map(parse_query)
     fc7 = sc.textFile(f_fc7).map(parse_fc7)
+    result = queries.map(lambda (p_i, p_j, c_ij): (p_i, (p_j, c_ij))) \
+                .join(fc7) \
+                .map(lambda (p_i, ((p_j, c_ij), fc7_i)): (p_j, (p_i, c_ij, fc7_i))) \
+                .groupByKey() \
+                .join(fc7) \
+                .map(lambda (p_j, (tuples, fc7_j)): (p_j, fc7_j, tuples)) \
+                .flatMap(get_cosine_similarity) \
+                .groupByKey() \
+                .map(expand_result)
 
-    result = queries.map(lambda (p_i, p_j, c_ij): ((p_i,), (p_i, p_j, c_ij)))\
-                .join(fc7) \
-                .map(lambda ((p_i, ), ((_, p_j, c_ij), fc7_i)): ((p_j,), (p_i, c_ij, fc7_i))) \
-                .join(fc7) \
-                .map(lambda ((p_j, ), ((p_i, c_ij, fc7_i), fc7_j)): (p_i, p_j, c_ij, fc7_i, fc7_j)) \
-                .map(get_cosine_similarity)
+    # print result.collect()
     if local_mode:
-        for i in sorted(result.collect(), key=lambda x: -x[2]):
+        for i in result.collect():
             print i
     else:
         result.saveAsTextFile(f_output)
