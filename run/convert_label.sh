e0fa1b21cbf41d557d27d4843dbde9fa080daeda
Heqing Ya <normanyahq@gmail.com>
2016-12-09 15:41:06 -0500

    modify script

--- run/convert_label.sh
+++ run/convert_label.sh
@@ -1,7 +1,11 @@
 source $HOME/capstone/Break12306Captcha/run/common.sh
+mkdir -p $DATA_PATH/tmp_label_prob
 python $SRC_PATH/imageprocess/label_mapper.py \
   $MODEL \
   $FILE_LIST \
   $CAPTCHA_IMAGE \
-  $LABEL_PROB \
+  $DATA_PATH/tmp_label_prob \
   8
+
+cat $DATA_PATH/tmp_label_prob/*.txt > $LABEL_PROB
+rm -r $DATA_PATH/tmp_label_prob
