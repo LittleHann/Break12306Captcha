95ad109abf06b8b0144ef8ceb0a7fee021ff2446
Haonan Sun <haonans@outlook.com>
2016-12-12 16:03:56 -0500

    more

--- data/evaluator.py
+++ data/evaluator.py
@@ -15,7 +15,7 @@ with open('labels.txt') as reader:
     labels = map(lambda line: line.strip(), reader)
 
 top1_count = top3_count = top5_count = 0
-
+count = 0
 assert os.path.isfile(args.prediction)
 with open(args.prediction) as reader:
     for i, line in enumerate(reader):
@@ -23,6 +23,7 @@ with open(args.prediction) as reader:
         predictions = map(lambda t: labels.index(str(t[0].encode('utf-8'))), eval(_predictions))
 
         if rgb_hash in ground_truth:
+            count += 1
             true_label = ground_truth[rgb_hash]
             if true_label in predictions[:5]:
                 top5_count += 1
@@ -31,8 +32,8 @@ with open(args.prediction) as reader:
             if true_label in predictions[:1]:
                 top1_count += 1
             print rgb_hash, true_label, predictions
-            print top1_count, top3_count, top5_count
+            print top1_count, top3_count, top5_count, count
 
-print 'Top 1: {} / 200 = {}'.format(top1_count, top1_count / 200.0)
-print 'Top 3: {} / 200 = {}'.format(top3_count, top3_count / 200.0)
-print 'Top 5: {} / 200 = {}'.format(top5_count, top5_count / 200.0)
+print 'Top 1: {} / {} = {}'.format(top1_count, count, count / 200.0)
+print 'Top 3: {} / {} = {}'.format(top3_count, count, count / 200.0)
+print 'Top 5: {} / {} = {}'.format(top1_count, count, count / 200.0)
