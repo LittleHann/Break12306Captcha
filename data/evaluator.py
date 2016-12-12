5762813b5fa9cd4d0d486289a333a1906851f57f
Ubuntu <ubuntu@ip-172-31-12-53.ec2.internal>
2016-12-12 20:32:46 +0000

    debug

--- data/evaluator.py
+++ data/evaluator.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+from operator import itemgetter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('prediction')
@@ -15,18 +16,19 @@ top1_count = top3_count = top5_count = 0
 
 assert os.path.isfile(args.prediction)
 with open(args.prediction) as reader:
-    for line in reader:
-        rgb_hash, _predictions = line.split('\t')
-        predictions = eval(_predictions)
+    for i, line in enumerate(reader):
+	rgb_hash, _predictions = line.split('\t')
+	predictions = map(itemgetter[0], eval(_predictions))
+	print predictions
         if rgb_hash in ground_truth:
-            true_label = ground_truth[rgb_hash]
+	    true_label = ground_truth[rgb_hash]
             if true_label in predictions[:5]:
                 top5_count += 1
             if true_label in predictions[:3]:
                 top3_count += 1
             if true_label in predictions[:1]:
                 top1_count += 1
-
+	    print rgb_hash, true_label, " ".join(predictions[:5])
 print 'Top 1: {} / 200 = {}'.format(top1_count, top1_count / 200.0)
 print 'Top 3: {} / 200 = {}'.format(top3_count, top3_count / 200.0)
 print 'Top 5: {} / 200 = {}'.format(top5_count, top5_count / 200.0)
