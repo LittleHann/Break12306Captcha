a3bf90fe4821d7eba1b0006e97a37711452a0ed9
Haonan Sun <haonans@outlook.com>
2016-12-12 02:46:04 -0500

    export datapath

--- run/flask_run.sh
+++ run/flask_run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source ./common.sh
+export DATA_PATH=/data2/haonans
 python ../image_searcher/app.py \
     $DATA_PATH/rgb_key_2_hashes.pickle \
     $DATA_PATH/hash_2_sources.pickle \
