48783cd386e4beea37e8e88e6978d827af3f5a43
Haonan Sun <haonans@outlook.com>
2016-12-12 03:23:51 -0500

    formatted

--- data/evaluator.py
+++ data/evaluator.py
@@ -1,22 +1,29 @@
 import os
 import logging
+import argparse
 
 logging.basicConfig(level=logging.INFO)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('prediction', help='path to prediction file')
+parser.add_argument('truth', help='path to ground truth number file')
+
+args = parser.parse_args()
+
 # Ground Truth
 ground_truth = {}
-with open('ground_truth.number') as reader:
+with open(args.truth) as reader:
     for line in reader:
         rgb_hash, i_label = line.strip().split()
         ground_truth[rgb_hash] = int(i_label)
 
 # Evaluation
-prediction_path = 'head_2000.txt'
-assert os.path.isfile(prediction_path)
 
-with open(prediction_path) as reader:
+with open(args.prediction) as reader:
     correct_counter = 0
     for i_line, line in enumerate(reader):
+        if i_line == 200:
+            break
         rgb_hash, i_prediction = eval(line.strip())
         assert rgb_hash in ground_truth
         i_truth = ground_truth[rgb_hash]
