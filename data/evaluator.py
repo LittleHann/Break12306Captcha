3cc2264e61ab13266e1b45aa4b93a27fab1c60f1
Haonan Sun <haonans@outlook.com>
2016-12-12 15:53:30 -0500

    debug

--- data/evaluator.py
+++ data/evaluator.py
@@ -30,7 +30,7 @@ with open(args.prediction) as reader:
                 top3_count += 1
             if true_label in predictions[:1]:
                 top1_count += 1
-            print rgb_hash, true_label, " ".join(predictions[:5])
+            print rgb_hash, true_label, predictions
 print 'Top 1: {} / 200 = {}'.format(top1_count, top1_count / 200.0)
 print 'Top 3: {} / 200 = {}'.format(top3_count, top3_count / 200.0)
 print 'Top 5: {} / 200 = {}'.format(top5_count, top5_count / 200.0)
